2ac06b1b9dca90c196063bcc95e31acb2195a8d5
Apl064 <apl_064@me.com>
2022-03-16 23:04:58 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Pavel Metelin on 3/15/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet var chosenAnimalLabel: UILabel!
+    @IBOutlet var chosenAnimalDescLabel: UILabel!
+        
+    var answersChosen: [Answer]!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.setHidesBackButton(true, animated: false)
+        
+        chosenAnimalLabel.text = "Вы - \(getChosenAnimal(from: answersChosen).rawValue)!"
+        chosenAnimalDescLabel.text = getChosenAnimal(from: answersChosen).definition
+    }
+    
+    private func getChosenAnimal(from answers: [Answer]) -> Animal {
+        let chosenAnimal = answersChosen.map {$0.animal}
+            .reduce(into: [:]) {counts, animal in
+                counts[animal, default: 0] += 1
+            }
+            .sorted{$0.value > $1.value}
+            .first?.key
+        
+        return chosenAnimal ?? .dog
+    }
+}
